b9f8abc3634e66680815235dc577a7c3c5e05acc
alobatonavantica <alvaro.lobaton@avantica.com>
2021-09-28 07:06:25 -0500

    Initial commit

---
+++ src/test/java/com/co/dockermvnplugin/DockermvnpluginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.dockermvnplugin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockermvnpluginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
